b9503859863b301efb6c5c32d122ddfc2cf0830e
ThillaimanyLogini <lathuthillai97@gmail.com>
2025-02-08 21:43:25 +0530

    menu_guest.php added

---
+++ menu_guest.php
@@ -0,0 +1,352 @@
+<ul class="nav nav-secondary">
+              <li class="nav-item">
+                <a data-bs-toggle="collapse" href="#dashboard">
+                  <i class="fas fa-home"></i>
+                  <p>Guest</p>
+                  <span class="caret"></span>
+                </a>
+                <div class="collapse" id="dashboard">
+                  <ul class="nav nav-collapse">
+                    <li>
+                      <a href="../demo1/index.php">
+                        <span class="sub-item">Dashboard 1</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="nav-section">
+                <span class="sidebar-mini-icon">
+                  <i class="fa fa-ellipsis-h"></i>
+                </span>
+                <h4 class="text-section">Components</h4>
+              </li>
+              <li class="nav-item active">
+                <a data-bs-toggle="collapse" href="#base" class="collapsed" aria-expanded="true">
+                  <i class="fas fa-layer-group"></i>
+                  <p>Base</p>
+                  <span class="caret"></span>
+                </a>
+                <div class="collapse" id="base">
+                  <ul class="nav nav-collapse">
+                    <li>
+                      <a href="index.php?page=staff.php&option=view">
+                        <span class="sub-item">Staff</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=customer.php&option=view">
+                        <span class="sub-item">Customer</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=supplier.php&option=view">
+                        <span class="sub-item">Supplier</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=advertisement.php&option=view">
+                        <span class="sub-item">Advertisement</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=booking.php&option=view">
+                        <span class="sub-item">Booking</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="index.php?page=bookingallocatestaff.php&option=view">
+                        <span class="sub-item">Staff allocate for Booking</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=bookingpackage.php&option=view">
+                        <span class="sub-item">Booking package</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=bookingsales.php&option=view">
+                        <span class="sub-item">Sales at booking</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=message.php&option=view">
+                        <span class="sub-item">message</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=package.php&option=view">
+                        <span class="sub-item">package</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=packagecategory.php&option=view">
+                        <span class="sub-item">Package catoegory</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=packagephoto.php&option=view">
+                        <span class="sub-item">Package photo</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=packageprice.php&option=view">
+                        <span class="sub-item">Package price</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=packageproduct.php&option=view">
+                        <span class="sub-item">Package product</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=packagesubcategory.php&option=view">
+                        <span class="sub-item">Package subcategory</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=payment.php&option=view">
+                        <span class="sub-item">Payment</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=product.php&option=view">
+                        <span class="sub-item">Product</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=productphoto.php&option=view">
+                        <span class="sub-item">product photo</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=productprice.php&option=view">
+                        <span class="sub-item">Product price</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=purchase.php&option=view">
+                        <span class="sub-item">purchase</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=purchaseproduct.php&option=view">
+                        <span class="sub-item">Purchase product</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=review.php&option=view">
+                        <span class="sub-item">Review</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=specialtime.php&option=view">
+                        <span class="sub-item">Shop open at special days</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=staffleave.php&option=view">
+                        <span class="sub-item">Staff leave</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=staffpackage.php&option=view">
+                        <span class="sub-item">staff allocated package</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=stock.php&option=view">
+                        <span class="sub-item">stock</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=stockne.php&option=view">
+                        <span class="sub-item">stock not expired</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=supplytoserviceitem.php&option=view">
+                        <span class="sub-item">Items supply to service </span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="index.php?page=supplytoservice.php&option=view">
+                        <span class="sub-item">supply to service </span>
+                      </a>
+                    </li>
+							
+
+				 </ul>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarLayouts">
+                  <i class="fas fa-th-list"></i>
+                  <p>Login</p>
+                  <span class="caret"></span>
+                </a>
+                <div class="collapse" id="sidebarLayouts">
+                  <ul class="nav nav-collapse">
+                    <li>
+                      <a href="index.php?page=login.php">
+                        <span class="sub-item">Login</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="index.php?page=forgetpassword.php">
+                        <span class="sub-item">Forget password</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="icon-menu.html">
+                        <span class="sub-item">Icon Menu</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a data-bs-toggle="collapse" href="#forms">
+                  <i class="fas fa-pen-square"></i>
+                  <p>Forms</p>
+                  <span class="caret"></span>
+                </a>
+                <div class="collapse" id="forms">
+                  <ul class="nav nav-collapse">
+                    <li>
+                      <a href="forms/forms.html">
+                        <span class="sub-item">Basic Form</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a data-bs-toggle="collapse" href="#tables">
+                  <i class="fas fa-table"></i>
+                  <p>Tables</p>
+                  <span class="caret"></span>
+                </a>
+                <div class="collapse" id="tables">
+                  <ul class="nav nav-collapse">
+                    <li>
+                      <a href="tables/tables.html">
+                        <span class="sub-item">Basic Table</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="tables/datatables.html">
+                        <span class="sub-item">Datatables</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a data-bs-toggle="collapse" href="#maps">
+                  <i class="fas fa-map-marker-alt"></i>
+                  <p>Maps</p>
+                  <span class="caret"></span>
+                </a>
+                <div class="collapse" id="maps">
+                  <ul class="nav nav-collapse">
+                    <li>
+                      <a href="maps/googlemaps.html">
+                        <span class="sub-item">Google Maps</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="maps/jsvectormap.html">
+                        <span class="sub-item">Jsvectormap</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a data-bs-toggle="collapse" href="#charts">
+                  <i class="far fa-chart-bar"></i>
+                  <p>Charts</p>
+                  <span class="caret"></span>
+                </a>
+                <div class="collapse" id="charts">
+                  <ul class="nav nav-collapse">
+                    <li>
+                      <a href="charts/charts.html">
+                        <span class="sub-item">Chart Js</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="charts/sparkline.html">
+                        <span class="sub-item">Sparkline</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a href="widgets.html">
+                  <i class="fas fa-desktop"></i>
+                  <p>Widgets</p>
+                  <span class="badge badge-success">4</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../../documentation/index.php">
+                  <i class="fas fa-file"></i>
+                  <p>Documentation</p>
+                  <span class="badge badge-secondary">1</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a data-bs-toggle="collapse" href="#submenu">
+                  <i class="fas fa-bars"></i>
+                  <p>Menu Levels</p>
+                  <span class="caret"></span>
+                </a>
+                <div class="collapse" id="submenu">
+                  <ul class="nav nav-collapse">
+                    <li>
+                      <a data-bs-toggle="collapse" href="#subnav1">
+                        <span class="sub-item">Level 1</span>
+                        <span class="caret"></span>
+                      </a>
+                      <div class="collapse" id="subnav1">
+                        <ul class="nav nav-collapse subnav">
+                          <li>
+                            <a href="#">
+                              <span class="sub-item">Level 2</span>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="#">
+                              <span class="sub-item">Level 2</span>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                    <li>
+                      <a data-bs-toggle="collapse" href="#subnav2">
+                        <span class="sub-item">Level 1</span>
+                        <span class="caret"></span>
+                      </a>
+                      <div class="collapse" id="subnav2">
+                        <ul class="nav nav-collapse subnav">
+                          <li>
+                            <a href="#">
+                              <span class="sub-item">Level 2</span>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <span class="sub-item">Level 1</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
